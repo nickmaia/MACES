b97da299ef03b47aa84d2a709819922ec860d7ee
nickmaia <nicolemaiaargondizzi02@gmail.com>
2024-08-24 20:56:43 -0300

    atualizando para deploy na render

--- backend/settings.py
+++ backend/settings.py
@@ -120,10 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-#STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 
 # Default primary key field type
