1fad44e837c97f1cf78e62a96de68450ceaa5538
nickmaia <nicolemaiaargondizzi02@gmail.com>
2024-08-24 21:05:41 -0300

    atualizando para deploy na render

--- staticfiles/accounts/js/transition.c78ff6d17313.js
+++
@@ -1,8 +0,0 @@
-// transition.js
-function transitionToPage(targetUrl) {
-    const container = document.querySelector('.container');
-    container.style.transform = 'translateX(-100%)'; // ou outro efeito desejado
-    setTimeout(() => {
-        window.location.href = targetUrl;
-    }, 500); // Corresponde ao tempo da transição CSS
-}
--- staticfiles/simulation/js/base.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('sidebarToggle').onclick = function () {
-    document.getElementById('sidebar').classList.toggle('active');
-    document.querySelector('.wrapper').classList.toggle('active');
-};
